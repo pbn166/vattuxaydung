96a2785a35ab07c066490f9b608e86e6cb831e8a
ThuThuy0701 <89577125+ThuThuy0701@users.noreply.github.com>
2022-04-20 13:26:19 +0700

    Add files via upload

--- view/client/tintuc.php
+++ view/client/tintuc.php
@@ -2,11 +2,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="http://localhost/santmdt/public/img/logo1.png">
+    <link rel="icon" type="image/x-icon" href="http://localhost/vatlxd/public/img/logo.png">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
-    <link rel="stylesheet" href="http://localhost/santmdt/public/css/layouts.css">
-    <link rel="stylesheet" href="http://localhost/santmdt/public/css/tintuc.css">
-    <title>Trang chủ - Sàn Thương mại số 1 Việt Nam</title>
+    <link rel="stylesheet" href="http://localhost/vatlxd/public/css/layouts.css">
+    
+    
+    <title>Tin tức</title>
 </head>
 
 <style>
@@ -47,20 +48,24 @@
 
 .hinhanh{
     flex-basis: 65%;
+   
+    
     position: relative;
     box-shadow: 5px 5px 3px #ffffff;
 }
 .the_a{
     position: absolute;
+    
+
     width: 100%;
-    background-color: #A8CA27;
+    background-color: orange;
     height: 15%;
     top: 0;
     
 }
 .baiviet-img{
     width: 50%;
-    height: 85%;
+    height: 80%;
     position: absolute;
     bottom: 0;
     left: 0;
@@ -89,15 +94,37 @@
 
 .danhmuc{
     text-align: center;
-    font-size: 25px;
+    font-size: 20px;
     height: auto;
 }
 
 .meow{
-    background-color: #A8CA27;   
+    background-color: orange;   
     margin-left: 90px;
     margin-right: 90px;
-    border-radius: 20px
+    border-radius: 20px;
+    text-align: justify;
+    padding-left: 10px ;
+    padding-bottom: 10px;
+    padding-right:10px ;
+}
+a{
+    color: brown;
+}
+.meow a{
+    color:white;
+}
+
+.meow a:hover{
+    color: brown;
+}
+
+.the_p a{
+    color: black;
+}
+
+.the_p a:hover{
+    color: orange;
 }
 </style>
 
@@ -113,97 +140,50 @@
             </div>
             <br>
             <br>
-
-
-
-
             <!-- SIDEBAR -->
             <div class="b">
                 <article class="sidebar">
                     <div class="h">
-                        <h1><center>Danh Mục Bài Viết</center></h1>
+                        <h1><center>Các loại tin tức</center></h1>
                     </div>
+                    <br>
                     <div class="danhmuc">
                         <div class="meow">
                             <ul>
-                                <u><b>Tin Tức Nông Nghiệp</b></u>
-                                <li>
-                                    Mãng Cầu
-                                </li>
-
-                                <li>
-                                    Cà Phê 10tấn/ha
-                                </li>
-
-                                <li>
-                                    Làng hoa Cái Mơn sau bão
-                                </li>
-
-                                <li>
-                                    
-                                </li>
+                                <b><center>Sự kiện</b></center>
+                                <li><a href="http://localhost/vatlxd/?router=sukien1">1.Khai mạc Tuần lễ giao thương quốc tế đồ gỗ và nội thất 2022</a></li>
+                                <li><a href="http://localhost/vatlxd/?router=sukien2">2.Báo cáo thị trường xi măng quý I/2022, bao gồm nội dung gì?</a></li>
                             </ul>
-
                             <ul>
-                                <u><b>Giải Pháp Kỹ Thuật</b></u>
-                                <li>
-                                    Xoài
-                                </li>
-                                <li>
-                                    Cà Phê
-                                </li>
-                                <li>
-                                    Hoa Cúc
-                                </li>
-                                <li>
-                                    Hoa Lan
-                                </li>
-                                <li>
-                                    Hoa Mai
-                                </li>
-                                <li>
-                                    Hoa Hồng
-                                </li>
-                                <li>
-                                    Hướng Dương
-                                </li>
-                                <li>
-                                    Hoa Mơ (Thịt Chó)
-                                </li>
+                                <b><center>Kinh doanh - đầu tư</center></b>
+                                <li><a href="http://localhost/vatlxd/?router=kinhdoanh1">1.Hiệp hội Thép Thế giới dự báo nhu cầu sử dụng thép chỉ tăng 0,4% trong năm 2022</a></li>
+                                <li><a href="http://localhost/vatlxd/?router=kinhdoanh2">2.Thị trường năng lượng Thế giới tồn tại nhiều bất ổn</a></li>
                             </ul>
                         </div>
                     </div>
+                   
                 </article>
-                <!-- SIDEBAR -->
 
-
-
+            <!-- SIDEBAR -->
             <!-- BÀI VIẾT -->
                 <article class="hinhanh">
-                    <a class="the_a">
+                    <div class="the_a">
                         <br>
-                        <h2><center>QUY TRÌNH TRỒNG VÀ CHẮM SÓC CÂY MAI ĐÚNG KỸ THUẬT</center></h2>
-                    </a>
-                    <img class="baiviet-img" src="http://localhost/santmdt/public/img/hoa-mai-vang.jpg" alt="">
-                    <p class="the_p">Mai có xuất xứ từ cây hoang dại, có khả năng thích ứng tốt với điều kiện khí hậu nhiệt đới,
-                        đặc biệt với khí hậu miền Nam. Cây mai sinh trưởng và phát triển mạnh,
-                        có tuổi thọ cao và nếu được chăm sóc chu đáo sẽ cho hoa nhiều và có màu sắc đẹp.
+                        <h2><center>ĐẰNG SAU CÂU CHUYỆN HÀNG TỒN KHO</center></h2>
+                    </div>
+                    <img class="baiviet-img" src="http://localhost/vatlxd/public/img/tintuc2.png" alt="">
+                    <p class="the_p">Hàng tồn kho mặc dù đã giảm dần trong thời gian qua nhưng cũng kéo theo sự suy giảm đáng lo ngại
+                         về chỉ số sản xuất của doanh nghiệp, đồng thời cho thấy doanh nghiệp trong nước có thể...
                         <br>
-                        <a href="http://localhost/santmdt/tintuc/html/hoamai.php">
+                        <a href="http://localhost/vatlxd/?router=tintuc2">
                             >>Xem Thêm
                         </a>
                         <br>
-                        <a>
-                            22. 02. 2022
-                            <br>
-                            <br>
-                            <b>Cửa Hàng Vật Tư Nông Nghiệp Phạm Ngọc Quyền</b>
-                        </a>
                     </p>
 
                 </article>  
-                
-            </div>
+
+</div>
             <br>
 
 
@@ -215,28 +195,20 @@
                 </article>
 
                 <article class="hinhanh">
-                    <a class="the_a">
+                    <div class="the_a">
                         <br>
-                        <h2><center>QUY TRÌNH TRỒNG VÀ CHẮM SÓC CÂY CÀ PHÊ ĐÚNG KỸ THUẬT</center></h2>
-                    </a>
-                    <img class="baiviet-img" src="http://localhost/santmdt/public/img/ky_thuat_trong_ca_phe.jpg" alt="">
-                    <p class="the_p">Giới thiệu quy trình kỹ thuật trồng cà phê, hướng dẫn chăm sóc cây cà phê cho năng suất cao và ổn định. 
-            Ngoài việc lựa chọn giống cà phê đạt tiêu chuẩn, có xuất xứ rõ ràng, 
-            thì kỹ thuật trồng cũng quyết định không nhỏ đến sinh trưởng và năng suất của vườn cà phê. 
+                        <h2><center>GIÁ THÉP BẮT ĐẦU TĂNG THEO GIÁ ĐIỆN</center></h2>
+                    </div>
+                    <img class="baiviet-img" src="http://localhost/vatlxd/public/img/tintuc1.png" alt="">
+                    <p class="the_p">Một số nhà thầu xây dựng, đại lý sắt thép và nhà sản xuất thép tại TPHCM cho biết giá thép xây dựng tăng 150.000 đồng/tấn kể từ hôm nay do tác động của giá đầu vào như điện, 
+                        chi phí vận chuyển..
                         <br>
-                        <a href="http://localhost/santmdt/tintuc/html/cf.php">
+                        <a href="http://localhost/vatlxd/?router=tintuc1">
                             >>Xem Thêm
-                        </a>
-                        <br>
-                        <a>
-                            22. 02. 2022
-                            <br>
-                            <br>
-                            <b> Cửa Hàng Vật Tư Nông Nghiệp Phạm Ngọc Quyền</b>
                         </a>
                     </p>
                 </article>
-            </div>
+</div>
             <br>
             
 
@@ -248,28 +220,22 @@
                 </article>
 
                 <article class="hinhanh">
-                    <a class="the_a">
+                    <div class="the_a">
                         <br>
-                        <h2><center>GIỚI THIỆU QUY TRÌNH TRỒNG XOÀI, CHĂM SÓC CÂY XOÀI</center></h2>
-                    </a>
-                    <img class="baiviet-img" src="http://localhost/santmdt/public/img/cay-xoai-6.jpg" alt="">
-                    <p class="the_p">Giới thiệu quy trình kỹ thuật trồng cà phê, hướng dẫn chăm sóc cây cà phê cho năng suất cao và ổn định.
-                        Ngoài việc lựa chọn giống cà phê đạt tiêu chuẩn, có xuất xứ rõ ràng, thì kỹ thuật trồng cũng quyết định không nhỏ đến sinh trưởng và năng suất của vườn cà phê.
+                        <h2><center>VẬT LIỆU XÂY DỰNG ĐIÊU ĐỨNG</center></h2>
+                    </div>
+                    <img class="baiviet-img" src="http://localhost/vatlxd/public/img/tintuc3.png" alt="">
+                    <p class="the_p">
+                    Đầu ra thu hẹp, bị chiếm dụng vốn, hàng ế kéo dài... nên các doanh nghiệp ngành xây dựng và vật liệu xây dựng đều lao đao.
                         <br>
-                        <a href="http://localhost/santmdt/tintuc/html/xoai.php">
+                        <a href="http://localhost/vatlxd/?router=tintuc3">
                             >>Xem Thêm
                         </a>
                         <br>
-                        <a>
-                            22. 02. 2022
-                            <br>
-                            <br>
-                            <b>Cửa Hàng Vật Tư Nông Nghiệp Phạm Ngọc Quyền</b>
-                        </a>
                     </p>
                 </article>
+</div>
 
-            </div>
             <br>
             <!-- END BÀI VIẾT -->
 
@@ -277,14 +243,13 @@
 
 
 
-            <div class="tt">
-                <h4>Tiếp theo<h4>
-            </div>
-        </div>
-
-    <!-- footer -->
-    <?php include('./view/client/layout/footer.php'); ?>
-    <!-- End footer -->
+           
 </div>
+
+    
+</div>
+<!-- footer -->
+<?php include('./view/client/layout/footer.php'); ?>
+    <!-- End footer -->
 </body>
 </html>
