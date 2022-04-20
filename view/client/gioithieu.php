2ca18bc5691d8b75073082ca4f3dee12b604fc9b
minhchy <101682878+minhchy@users.noreply.github.com>
2022-04-20 14:06:55 +0700

    Add files via upload

---
+++ view/client/gioithieu.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="http://localhost/vatlxd/public/img/logo.png">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link rel="stylesheet" href="http://localhost/vatlxd/public/css/home.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="http://localhost/vatlxd/public/css/layouts.css">
+    <title>Giới Thiệu</title>
+</head>
+
+<body>
+    <div class="wapper">
+        <!-- header -->
+        <?php include('./view/client/layout/header.php'); ?>
+        <!-- End header -->
+
+        <!-- Content -->
+        <!-- Code trang chu o day -->
+        <article style="padding: 30px" >
+        <div style="font-size: 24px; " >
+        <strong>Đôi nét về công ty chúng tôi</strong> <br> <br>
+        <p>Với mong muốn tạo sự vững chắc và hoàn mỹ cho từng công trình, chúng tôi luôn chú trọng đến từng nguyên liệu, chất liệu và chi tiết sản phẩm để có thể đem lại sự hài lòng tuyệt đối cho các đối tác cũng như người sử dụng.</p> <br>  
+        <p>Công ty cổ phần đầu tư xây dựng công nghệ cao Việt Nam tự hào là doanh nghiệp tiên phong trong lĩnh vực cung cấp nguồn nguyên liệu xây dựng uy tín và chất lượng hàng đầu việt với hệ thống chi nhánh trải dài khắp Việt Nam</p> <br>
+        <p>Quý khách đến với VITC sẽ được chia sẻ và đảm bảo quyền lợi:</p> <br>
+        <div style="font-size: 20px;" >
+        <p>-Tư vấn bán hàng, tư vấn sử dụng</p> <br>
+        <p>-Sản phẩm chất lượng và giá cả hợp lý</p> <br>
+        <p>-Hoàn hảo tiện ích dịch vụ cung cấp</p> <br>
+        </div>
+        </p>
+        </article>
+        <!-- <div id="content3">
+                best seller
+            </div> -->
+        <!-- End content -->
+
+        <!-- footer -->
+        <?php include('./view/client/layout/footer.php'); ?>
+        <!-- End footer -->
+    </div>
+
+</body>
+
+</html>
---
+++ view/client/lienhe.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="http://localhost/vatlxd/public/img/logo.png">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link rel="stylesheet" href="http://localhost/vatlxd/public/css/home.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="http://localhost/vatlxd/public/css/layouts.css">
+    <title>Liên Hệ</title>
+</head>
+
+<body>
+    <div class="wapper">
+        <!-- header -->
+        <?php include('./view/client/layout/header.php'); ?>
+        <!-- End header -->
+
+        <!-- Content -->
+        <!-- Code trang chu o day -->
+        <article>
+        <div style="text-align: center;">
+        <p>  </p>
+        <em>GIÁ CẢ SẼ THAY ĐỔI THEO THỜI GIAN DO NGUYÊN VẬT LIỆU CÓ THỂ TĂNG HOẶC GIẢM GIÁ THEO TỪNG THỜI ĐIỂM, ĐỂ BIẾT GIÁ CỤ THỂ VÀ CHÍNH XÁC NHẤT VUI LÒNG ĐẾN TRỰC TIẾP CỬA HÀNG THEO BẢN ĐỒ TRÊN GOOGLE MAP DƯỚI ĐÂY HOẶC LIÊN HỆ CHÚNG TÔI QUA ZALO/SĐT : 0909000000 </em> <br>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30756.442816140127!2d105.77141778452346!3d10.038281675387207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a0895a51d60719%3A0x9d76b0035f6d53d0!2zxJDhuqFpIGjhu41jIEPhuqduIFRoxqE!5e0!3m2!1svi!2sus!4v1650372682375!5m2!1svi!2sus" width="1200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <div class="fck_detail">
+	
+    <p><span style="color:#2980b9;"><strong><span style="font: size 32px;px;"><span style="font-family:times new roman,times,serif;">CÔNG TY CỔ PHÂN XÂY DỰNG CÔNG NGHỆ CAO VIỆT NAM&nbsp;VITC</span></span></strong></span></p>
+
+<p><span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">Trụ sở chính:&nbsp; &nbsp; 68/10 Trần Phú, Cái Khế, Ninh Kiều, Cần Thơ .</span></span></p>
+
+<p><font face="times new roman, times, serif"><span style="font-size: 16px;">VP&nbsp; &nbsp; &nbsp; &nbsp;:&nbsp; &nbsp; 485</span></font><span style="font-family: &quot;times new roman&quot;, times, serif; font-size: 16px;">&nbsp;3/2&nbsp;- Ninh Kiều&nbsp;- Tp. Cần Thơ.</span></p>
+
+<p><span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">Hotline:&nbsp; &nbsp;0798003703</span></span></p>
+
+<p><span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">Email&nbsp; &nbsp;:&nbsp; &nbsp; sales@vitc.vn</span></span><span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">&nbsp; &nbsp;-&nbsp; &nbsp; info@vitc.vn</span></span></p>
+
+</div>
+        </div>
+        
+        </article>
+        <!-- <div id="content3">
+                best seller
+            </div> -->
+        <!-- End content -->
+            				
+
+
+        <!-- footer -->
+        <?php include('./view/client/layout/footer.php'); ?>
+        <!-- End footer -->
+    </div>
+
+</body>
+
+</html>
