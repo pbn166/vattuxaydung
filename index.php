2d08a039f292e6e5ac4c6c8343f5a18f4381fb89
ThuThuy0701 <89577125+ThuThuy0701@users.noreply.github.com>
2022-04-25 15:44:49 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
-<!-- file này ko ai được dụng tới -->
-
 <?php
 include './router/configRoute.php';
 if (isset($_GET['router'])) {
-    $route->view($_GET['router']);
+    if($_GET['router'] == 'admin') {
+        if(isset($_GET['page'])) {
+            $route->view($_GET['router'],$_GET['page']);
+        } else {
+            $route->view($_GET['router'],'trangchu');
+        }
+    }
+    else {
+        $route->view($_GET['router']);
+    }
 } else {
     include('./view/client/trangchu.php');
 }
